7adae6009af5b3d2a4408818fe82a092f4c459a5
José Luis Ávila <cloudluis22@hotmail.com>
2023-09-09 10:58:44 -0600

    AppBar con color, titulo e iconos superiores agregada.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( const MyWhatsapp() );
+  runApp(const MyWhatsapp());
 }
 
 class MyWhatsapp extends StatelessWidget {
@@ -12,14 +12,19 @@ class MyWhatsapp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.green,
+          backgroundColor: const Color.fromARGB(255, 18, 140, 126),
           title: const Text('WhatsApp'),
+          actions: const [
+            Icon(Icons.camera_alt_outlined),
+            SizedBox(width: 10,),
+            Icon(Icons.search),
+            SizedBox(width: 5,),
+            Icon(Icons.more_vert),
+            SizedBox(width: 10,),
+          ],
         ),
-        body: Container(
-          child: const Text('hola a todos'),
-        ),
+        body: const Text('Hola Mundo'),
       ),
     );
   }
 }
-
