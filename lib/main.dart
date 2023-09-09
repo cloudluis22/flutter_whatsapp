e6cc710927519c94cb1c3e17219527de3941fe0b
José Luis Ávila <cloudluis22@hotmail.com>
2023-09-09 14:24:28 -0600

    Agregado AppBar con opciones e iconos.

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,41 @@ class MyWhatsapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 18, 140, 126),
-          title: const Text('WhatsApp'),
-          actions: const [
-            Icon(Icons.camera_alt_outlined),
-            SizedBox(width: 10,),
-            Icon(Icons.search),
-            SizedBox(width: 5,),
-            Icon(Icons.more_vert),
-            SizedBox(width: 10,),
-          ],
+        home: DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(85),
+          child: AppBar(
+            backgroundColor: const Color.fromARGB(255, 7, 94, 84),
+            title: const Text('WhatsApp'),
+            actions: const [
+              Icon(Icons.camera_alt_outlined),
+              SizedBox(
+                width: 10,
+              ),
+              Icon(Icons.search),
+              SizedBox(
+                width: 5,
+              ),
+              Icon(Icons.more_vert),
+              SizedBox(
+                width: 10,
+              ),
+            ],
+            bottom: const TabBar(
+              tabs: [
+                Icon(Icons.groups),
+                Text('Chats'),
+                Text('Estados'),
+                Text('Llamadas'),
+              ],
+              indicatorColor: Color.fromARGB(255, 18, 140, 126),
+            ),
+          ),
         ),
-        body: const Text('Hola Mundo'),
+        body: const Text('Hola a todos mis amigos'),
       ),
-    );
+    ));
   }
 }
