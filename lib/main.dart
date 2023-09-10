a95b16768dd72d6589e105a3a14fefa5c77c0eb3
José Luis Ávila <cloudluis22@hotmail.com>
2023-09-10 09:16:07 -0600

    Agregado diseño de ListTile inicial.

--- lib/main.dart
+++ lib/main.dart
@@ -11,40 +11,58 @@ class MyWhatsapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(85),
-          child: AppBar(
-            backgroundColor: const Color.fromARGB(255, 7, 94, 84),
-            title: const Text('WhatsApp'),
-            actions: const [
-              Icon(Icons.camera_alt_outlined),
-              SizedBox(
-                width: 10,
+            length: 4,
+            initialIndex: 1,
+            child: Scaffold(
+              appBar: PreferredSize(
+                preferredSize: const Size.fromHeight(85),
+                child: AppBar(
+                  backgroundColor: const Color.fromARGB(255, 7, 94, 84),
+                  title: const Text('WhatsApp'),
+                  actions: const [
+                    Icon(Icons.camera_alt_outlined),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(Icons.search),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Icon(Icons.more_vert),
+                    SizedBox(
+                      width: 10,
+                    ),
+                  ],
+                  bottom: const TabBar(
+                    tabs: [
+                      Icon(Icons.groups),
+                      Text('Chats'),
+                      Text('Estados'),
+                      Text('Llamadas'),
+                    ],
+                    indicatorColor: Color.fromARGB(255, 18, 140, 126),
+                  ),
+                ),
               ),
-              Icon(Icons.search),
-              SizedBox(
-                width: 5,
+              body: Container(
+                margin: const EdgeInsets.only(top: 15),
+                child: ListView(
+                  children: const [
+                    ListTile(
+                      leading: CircleAvatar(
+                        radius: 45,
+                        backgroundImage: AssetImage('assets/amlol.jpeg'),
+                      ),
+                      title: Text('AMLOL XD'),
+                      subtitle: Text('Ya te deposité lo del mes mi amor.'),
+                      trailing: Column(
+                        children: [Text('7:56')],
+                      ),
+                      horizontalTitleGap: 0,
+                    )
+                  ],
+                ),
               ),
-              Icon(Icons.more_vert),
-              SizedBox(
-                width: 10,
-              ),
-            ],
-            bottom: const TabBar(
-              tabs: [
-                Icon(Icons.groups),
-                Text('Chats'),
-                Text('Estados'),
-                Text('Llamadas'),
-              ],
-              indicatorColor: Color.fromARGB(255, 18, 140, 126),
-            ),
-          ),
-        ),
-        body: const Text('Hola a todos mis amigos'),
-      ),
-    ));
+            )));
   }
 }
